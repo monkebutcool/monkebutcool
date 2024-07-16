aa16644152933d9efa5bb6ace2bc18ff083e4ef2
Monke But Cool <110347394+monkebutcool@users.noreply.github.com>
2024-07-16 17:19:06 +0300

    Create delall.py

---
+++ delall.py
@@ -0,0 +1,25 @@
+import os
+
+def list_directories():
+    path = '/data/data/com.termux/files/home/'
+    directories = [d for d in os.listdir(path) if os.path.isdir(os.path.join(path, d)) and not d.startswith('.')]
+    
+    for idx, directory in enumerate(directories, start=1):
+        print(f"[{idx}] /{directory}")
+    
+    print("[A] Author")
+    print("[E] Exit")
+    
+    choice = input("Choose Input: ")
+    
+    if choice.isdigit() and 1 <= int(choice) <= len(directories):
+        print(f"rm -rf {os.path.join(path, directories[int(choice) - 1])}")
+    elif choice == 'A':
+        print("[Blogspot](https://apkntermux.blogspot.com) {hypertext}")
+    elif choice == 'E':
+        print("CTRL+C")
+
+if __name__ == '__main__':
+    user_input = input("Enter 'delall' to run the program: ")
+    if user_input == 'delall':
+        list_directories()
